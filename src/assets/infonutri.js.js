0616c8b7fbff057a1be9ad1661805485ea9028a1
Camilla <camillaamaral7@gmail.com>
2021-10-28 15:17:58 -0300

    Json com dados da tabela nutricional.

---
+++ src/assets/infonutri.js.js
@@ -0,0 +1,43 @@
+const INFONUTRI_DATA = [
+  {
+    id: 1,
+    fruit: "morango",
+    calories: "50 kcal",
+    carb: "30 g",
+    ptn: "3 g",
+    total: "1 g",
+    fiber: "3 g",
+  },
+
+  {
+    id: 2,
+    fruit: "banana",
+    calories: "60 kcal",
+    carb: "30 g",
+    ptn: "3 g",
+    total: "1 g",
+    fiber: "3 g",
+  },
+
+  {
+    id: 3,
+    fruit: "melancia",
+    calories: "30 kcal",
+    carb: "30 g",
+    ptn: "3 g",
+    total: "1 g",
+    fiber: "3 g",
+  },
+
+  {
+    id: 4,
+    fruit: "manga",
+    calories: "50 kcal",
+    carb: "30 g",
+    ptn: "3 g",
+    total: "1 g",
+    fiber: "3 g",
+  },
+];
+
+export default INFONUTRI_DATA;
