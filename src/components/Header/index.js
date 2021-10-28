0caa4d1973082ed3438c167a956459c68102b3d1
Camilla <camillaamaral7@gmail.com>
2021-10-28 15:19:37 -0300

    Add componente Header.

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.css";
+import logo1 from "../../img/logo1.png";
+import Search from "../Search";
+import restaurante from "../../img/restaurante.png";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <img className="logo" src={logo1} />
+        
+        <img className="restaurant" src={restaurante} />
+        <h3>Economize e ganhe em saúde!</h3>
+        <nav>
+          <button className="btnNav">Início</button>
+          <button className="btnNav">Receitas</button>
+
+<Search />
+
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Header;
+
+
+//{this.props.showButton ? <button>Botão</button>}
+//PropType ---> dá pra dizer que a tabela é um array de objetos
