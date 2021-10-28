9e6eab2fff0ceae2d15b4e0e753e83677307246d
Camilla <camillaamaral7@gmail.com>
2021-10-28 15:15:47 -0300

    Criar class App, acrescentar componentes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from './pages/Home';
+import RecipePage from './pages/RecipePage';
+import Register from './pages/Register';
+import "./App.css";
+import PropTypes from "prop-types";
+import Header from "./components/Header";
+import Recipe from "./components/RecipeList/Recipe";
+import Table from "./components/Table";
+import Footer from "./components/Footer";
+import DATA from "./assets/recipe-data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  //handleChange = (event) => {
+  //const text = event.target.value
+  //DATA.filter((recipes) => {
+  //return recipes.title.toLowerCase().includes(text.toLowerCase());
+  //})
+  //this.setState({recipes: filterData})
+  //}
+
+  render() {
+    return (
+      <div>
+        <Header />
+
+        <h1 className="main-title">Receitas</h1>
+
+        <section className="container-recipes">
+          {DATA.map((item) => (
+            <Recipe
+              key={item.id}
+              image={item.image}
+              title={item.title}
+              subtitle={item.subtitle}
+            />
+          ))}
+        </section>
+
+        <section className="nutri-container">
+          <Table />
+          <Table />
+          <Table />
+          <Table />
+        </section>
+        <Footer />
+      </div>
+    );
+  }
 }
+
+Recipe.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  autor: PropTypes.string,
+  recipeText: PropTypes.string.isRequired,
+};
 
 export default App;
