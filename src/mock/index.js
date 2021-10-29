85adc1d50d9424cb43b71f9633a0bf78b63f29f2
Camilla <camillaamaral7@gmail.com>
2021-10-29 17:53:34 -0300

    Add createServer (mock).

---
+++ src/mock/index.js
@@ -0,0 +1,307 @@
+import { createServer } from "miragejs";
+
+const RANDOM_RECIPES = {
+  results: [
+    {
+      id: 1,
+      image:
+        "https://images.pexels.com/photos/3186961/pexels-photo-3186961.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+      title: "COMPOTA DE MORANGOS",
+      subtitle:
+        "Morango, limão e pimenta-do-reino. Pode reunir esse trio que dá supercerto!",
+      autor: "Palmirinha",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "10 minutos",
+      level: "Nível: fácil",
+      servings: "Rende 4 porções.",
+    },
+
+    {
+      id: 2,
+      image:
+        "https://images.pexels.com/photos/2684555/pexels-photo-2684555.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+      title: "BOLO DE CASCA DE BANANA",
+      subtitle:
+        "Para a cobertura, faça um caramelo com açúcar e água. Acrescente as bananas cortadas em rodelas e o suco de limão. Cubra o bolo ainda quente!!",
+      autor: "Ana Maria Braga",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "50 minutos",
+      level: "Nível: média",
+      servings: "Rende 6 porções.",
+    },
+
+    {
+      id: 3,
+      image:
+        "https://images.pexels.com/photos/5060453/pexels-photo-5060453.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+      title: "SORVETE DE MANGA COM CASCA",
+      subtitle:
+        "A tradicional sobremesa feita com esta fruta riquíssima em nutrientes e aproveitando a casca!",
+      autor: "Rita Lobo",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "60 minutos",
+      level: "Nível: difícil",
+      servings: "Rende 4 porções.",
+    },
+
+    {
+      id: 4,
+      image:
+        "https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+      title: "MACARRÃO COM RAMAS DE CENOURA",
+      subtitle:
+        "Use o refogado como molho para o macarrão cozido. Se quiser, acrescente queijo ralado!",
+      autor: "Ana Maria Braga",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "60 minutos",
+      level: "Nível: difícil",
+      servings: "Rende 4 porções.",
+    },
+
+    {
+      id: 5,
+      image:
+        "https://images.pexels.com/photos/7840652/pexels-photo-7840652.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+      title: "RISOTO DE CASCA DE ABÓBORA",
+      subtitle:
+        "Coloque o arroz já cozido, alternando com o creme de leite e o requeijão. Tempere com cheiro verde, sal e shoyu a gosto.",
+      autor: "Ana Maria Braga",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "60 minutos",
+      level: "Nível: difícil",
+      servings: "Rende 4 porções.",
+    },
+
+    {
+      id: 6,
+      image:
+        "https://images.pexels.com/photos/4197987/pexels-photo-4197987.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+      title: "PÃO DE CASCA DE BANANA",
+      subtitle:
+        "Que tal reaproveitar as cascas da banana para um delicioso pão?",
+      autor: "Ana Maria Braga",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "60 minutos",
+      level: "Nível: difícil",
+      servings: "Rende 4 porções.",
+    },
+
+    {
+      id: 7,
+      image:
+        "https://images.pexels.com/photos/3883509/pexels-photo-3883509.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+      title: "BRIGADEIRO DE CASCA DE BANANA",
+      subtitle:
+        "A receita é quase a mesma do brigadeiro comum. Mas o acréscimo da casca de banana adiciona nutrientes ao doce, como o potássio!",
+      autor: "Palmirinha",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "20 minutos",
+      level: "Nível: fácil",
+      servings: "Rende 2 porções.",
+    },
+
+    {
+      id: 8,
+      image:
+        "https://images.pexels.com/photos/1346347/pexels-photo-1346347.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+      title: "SUCO DE TALOS DE VERDURAS",
+      subtitle: "Você ainda pode usar o bagaço para fazer uma geléia!!",
+      autor: "Palmirinha",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "60 minutos",
+      level: "Nível: difícil",
+      servings: "Rende 4 porções.",
+    },
+
+    {
+      id: 9,
+      image:
+        "https://images.pexels.com/photos/8280002/pexels-photo-8280002.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+      title: "PASTEL ASSADO DE TALOS DE AGRIÃO",
+      subtitle:
+        "Para o recheio, refogue a cebola e o alho, acrescente os talos de agrião e continue refogando, adicionando, em seguida, a azeitona e sal a gosto.",
+      autor: "Ana Maria Braga",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "60 minutos",
+      level: "Nível: difícil",
+      servings: "Rende 4 porções.",
+    },
+
+    {
+      id: 10,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjTnREkxGZ-gmQi5GNJJZ3c-pAxrWz-cpvQnmT67CcPioPwX18okpUhJYPGHapy9t7zuU&usqp=CAU",
+      title: "APERITIVO DE CASCA DE BATATA",
+      subtitle:
+        "Cascas de batatas e sementes de abóbora preparadas dessa mesma forma também rendem bons aperitivos!!",
+      autor: "Rita Lobo",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "20 minutos",
+      level: "Nível: fácil",
+      servings: "Rende 4 porções.",
+    },
+
+    {
+      id: 11,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2FAyIQ6Ox2qL0EeUkg_AVckjFZSrCFfj5VJweD9h_vlPM_keJkMtv8vqLSSBwoOn9Lls&usqp=CAU",
+      title: "DOCE DE CASCA DE LARANJA",
+      subtitle:
+        "Corte as cascas em tirinhas de meio centímetro e deixe de molho, trocando a água a cada duas horas, até perderem o amargo.",
+      autor: "Ana Maria Braga",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "60 minutos",
+      level: "Nível: difícil",
+      servings: "Rende 4 porções.",
+    },
+
+    {
+      id: 13,
+      image:
+        "https://fastlife.fastshop.com.br/wp-content/uploads/2021/04/pate-caseiro-alho-poro.jpg",
+      title: "PATÊ DE FOLHAS COM ALHO PORÓ",
+      subtitle:
+        "É comum utilizarmos o talo branco do alho-poró em refogados, mas e as folhas? Elas podem render um delicioso patê!",
+      autor: "Palmirinha",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "30 minutos",
+      level: "Nível: fácil",
+      servings: "Rende 2 porções.",
+    },
+    {
+      id: 14,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2FAyIQ6Ox2qL0EeUkg_AVckjFZSrCFfj5VJweD9h_vlPM_keJkMtv8vqLSSBwoOn9Lls&usqp=CAU",
+      title: "DOCE DE CASCA DE LARANJA",
+      subtitle:
+        "Corte as cascas em tirinhas de meio centímetro e deixe de molho, trocando a água a cada duas horas, até perderem o amargo.",
+      autor: "Ana Maria Braga",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "60 minutos",
+      level: "Nível: difícil",
+      servings: "Rende 4 porções.",
+    },
+
+    {
+      id: 12,
+      image:
+        "https://fastlife.fastshop.com.br/wp-content/uploads/2021/04/pate-caseiro-alho-poro.jpg",
+      title: "PATÊ DE FOLHAS COM ALHO PORÓ",
+      subtitle:
+        "É comum utilizarmos o talo branco do alho-poró em refogados, mas e as folhas? Elas podem render um delicioso patê!",
+      autor: "Palmirinha",
+      ingredients: [
+        "500 g de morangos (cerca de 3 xícaras)",
+        "3 colheres (sopa) de açúcar",
+        "1 colher (sopa) de caldo de limão",
+        "pimenta-do-reino moída na hora a gosto",
+      ],
+      prepare:
+        "Preaqueça o forno a 200 ºC (temperatura média).Lave, descarte os cabinhos e corte os morangos maiores ao meio, no sentido do comprimento. Mantenha os morangos menores inteiros. Transfira os morangos para uma assadeira média, de preferência antiaderente. Polvilhe com o açúcar, regue com o caldo de limão e tempere com pimenta-do-reino moída na hora a gosto. Misture delicadamente com uma espátula. Leve ao forno para assar por cerca de 20 minutos, ou até que forme uma calda brilhante e os morangos fiquem macios.",
+      duration: "30 minutos",
+      level: "Nível: fácil",
+      servings: "Rende 2 porções.",
+    },
+  ],
+};
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/allrecipes", () => {
+      return RANDOM_RECIPES;
+    });
+  },
+});
