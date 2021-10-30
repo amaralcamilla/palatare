26df164d1642207c85418f3c38a2c00efc8ef8c4
Camilla <camillaamaral7@gmail.com>
2021-10-29 21:32:11 -0300

    Transferir cód. do App para Home.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,8 +3,20 @@ import Header from "../../components/Header";
 import Search from "../../components/Search";
 import RecipeList from "../../components/RecipeList";
 import Recipe from "../../components/RecipeList/Recipe";
+import Footer from "../../components/Footer";
+import DATA from "../../assets/recipe-data";
 
 class Home extends React.Component {
+
+  // handleChange = (event) => {
+  //   const text = event.target.value
+  //   DATA.filter((recipes) => {
+  //   return recipes.title.toLowerCase().includes(text.toLowerCase());
+  //   })
+  //   this.setState({recipes: filterData})
+  //   }
+
+
   constructor(props) {
     super(props);
 
@@ -42,24 +54,46 @@ class Home extends React.Component {
 
   render() {
     return (
-      <>
+
+<div>
         <Header />
 
-        <Search onChange={this.handleChange} />
+        <h1 className="main-title">R E C E I T A S</h1>
 
-        {this.state.isLoading && "Loading..."}
-        {!this.state.isLoading && (
-          <RecipeList>
-            {this.state.recipeList.map((rec) => (
-              <Recipe
-                photo={rec.photo}
-                title={rec.title}
-                subtitle={rec.subtitle}
-              />
-            ))}
-          </RecipeList>
-        )}
-      </>
+        <section className="container-recipes">
+          {DATA.map((item) => (
+            <Recipe
+              key={item.id}
+              image={item.image}
+              title={item.title}
+              subtitle={item.subtitle}
+            />
+          ))}
+        </section>
+
+
+
+        <Footer />
+      </div>
+
+      // <>
+      //   <Header />
+
+      //   <Search onChange={this.handleChange} />
+
+      //   {this.state.isLoading && "Loading..."}
+      //   {!this.state.isLoading && (
+      //     <RecipeList>
+      //       {this.state.recipeList.map((rec) => (
+      //         <Recipe
+      //           photo={rec.photo}
+      //           title={rec.title}
+      //           subtitle={rec.subtitle}
+      //         />
+      //       ))}
+      //     </RecipeList>
+      //   )}
+      // </>
     );
   }
 }
