6169961417995f9c261c15b8216303481865f0d3
Camilla <camillaamaral7@gmail.com>
2021-10-30 18:47:48 -0300

    Add estilo na lista de receitas.

--- src/components/RecipeList/index.js
+++ src/components/RecipeList/index.js
@@ -1,12 +1,9 @@
 import React from "react";
+import './style.css'
 
 class RecipeList extends React.Component {
   render() {
-    return (
-
-        <ul className="recipeList">{this.props.children}</ul>
-
-    );
+    return <ul className="recipeList">{this.props.children}</ul>;
   }
 }
 
