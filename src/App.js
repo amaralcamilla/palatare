b20b2e80628da50cd701e04ba975c08f622f0c61
Camilla <camillaamaral7@gmail.com>
2021-10-29 21:22:20 -0300

    Add estado home/recipePage.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,36 @@
 import React from "react";
-import Home from './pages/Home';
-import RecipePage from './pages/RecipePage';
-import Register from './pages/Register';
+import Home from "./pages/Home";
+import RecipePage from "./pages/RecipePage";
+import Register from "./pages/Register";
 import "./App.css";
 import PropTypes from "prop-types";
 import Header from "./components/Header";
 import Recipe from "./components/RecipeList/Recipe";
-import Table from "./components/Table";
-import Footer from "./components/Footer";
-import DATA from "./assets/recipe-data";
 
 class App extends React.Component {
-  //handleChange = (event) => {
-  //const text = event.target.value
-  //DATA.filter((recipes) => {
-  //return recipes.title.toLowerCase().includes(text.toLowerCase());
-  //})
-  //this.setState({recipes: filterData})
-  //}
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isHomePage: true,
+    };
+}
+
+  handleChangePage = () => {
+    this.setState({ isHomePage: !this.state.isHomePage });
+  };
 
   render() {
     return (
-      <div>
-        <Header />
-
-        <h1 className="main-title">R E C E I T A S</h1>
-
-        <section className="container-recipes">
-          {DATA.map((item) => (
-            <Recipe
-              key={item.id}
-              image={item.image}
-              title={item.title}
-              subtitle={item.subtitle}
-            />
-          ))}
-        </section>
-
-        <section className="nutri-container">
-          <Table />
-          <Table />
-          <Table />
-          <Table />
-        </section>
-
-        <RecipePage />
-
-        <Footer />
-      </div>
+      <>
+        {this.state.isHomePage && <Home onChangePage={this.handleChangePage} />}
+        {!this.state.isHomePage && (
+          <RecipePage onChangePage={this.handleChangePage} />
+        )}
+      </>
     );
   }
 }
 
-Recipe.propTypes = {
-  image: PropTypes.string,
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  autor: PropTypes.string,
-  recipeText: PropTypes.string.isRequired,
-};
 
 export default App;
