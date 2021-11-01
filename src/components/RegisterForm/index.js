31c05486db2919eb4bf2d9142141033c175026a9
Camilla <camillaamaral7@gmail.com>
2021-11-01 01:44:16 -0300

    Add state e deixar campos do registro dinâmicos.

--- src/components/RegisterForm/index.js
+++ src/components/RegisterForm/index.js
@@ -4,11 +4,31 @@ import "./style.css";
 class RegisterForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    {
+      /*1. Inicializando/definindo o state*/
+    }
+    this.state = { name: "", email: "", message: [] };
+    {
+      /*Para validação: Criar um estado para cada input (colocar em cada input {this.props.campo}. Faz com que possamos assumir o controle do input para poder usá-lo futuramente atrelando à uma API, por ex.*/
+    }
   }
-  //Cria evento quando clicar no botão submit.
+
+  handleChange = (event) => {
+    const { value, name } = event.target;
+
+    {
+      /*3. Atualizando o state*/
+    }
+    this.setState({ [name]: value });
+    {
+      /*Quando temos um objeto e queremos acessar uma chave dinâmica (que não temos ainda o valor), usamos [] com o nome da variável dentro. Este "name" que usamops dentro do handleChange, é um atributo do target, de qualquer propriedade do html. Não tem a ver com o "name" que definimos no input.*/
+    }
+  };
+
+  //Cria evento quando clicar no botão submit prevenir ação default (que no reload muda de página. Após o submit muda automaticamente para a página inicial.
   handleSubmit = (event) => {
     event.preventDefault();
+    console.log("handleSubmit");
   };
 
   render() {
@@ -22,19 +42,39 @@ class RegisterForm extends React.Component {
         />
         <form className="register-form" onSubmit={this.handleSubmit}>
           {" "}
-          //Prevenir ação default (que no reload muda de página. Após o submit
-          muda automaticamente para a página inicial.)
           <label className="data-label">
-            Nome completo:{" "}
-            <input className="data-input" type="text" name="name" required />
+            Nome:{" "}
+            <input
+              className="data-input"
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              required
+            />
+            {/*2. Utilizando o state (this.state)*/};
           </label>
           <label className="data-label">
             E-mail:{" "}
-            <input className="data-input" type="email" name="email" required />
+            <input
+              className="data-input"
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              required
+            />
           </label>
           <label className="data-label">
             Mensagem:{" "}
-            <textarea className="message" maxLength="250" rows="6" required />
+            <textarea
+              className="message"
+              maxLength="250"
+              rows="6"
+              value={this.state.message}
+              onChange={this.handleChange}
+              required
+            />
           </label>
           <input className="btn-submit" type="submit" value="Enviar" />
         </form>
