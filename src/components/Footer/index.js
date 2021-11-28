9967648901723344b34e5e50e56d8f1f7c45488e
Camilla <camillaamaral7@gmail.com>
2021-11-28 11:20:30 -0300

    Refatora Footer.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,36 +1,32 @@
-import React from "react";
-import "./style.css";
-import logo2 from "../../img/logo2.png";
-import { AiOutlineMail } from "react-icons/ai";
-import { BsTelephone } from "react-icons/bs";
-import { MdLocationOn } from "react-icons/md";
+import logo2 from "../../assets/logo2.png";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { MdLocationOn, MdMail } from "react-icons/md";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer>
-        <img className="logo2" src={logo2} />
-        <ul className="footerList">
-          <li className="footerItem">
-            <MdLocationOn className="icon" />
-            <p className="contact">
-              {" "}
-              Av. Brasil, 789 <br />
-              Baln. Camboriú | SC
-            </p>
-          </li>
-          <li className="footerItem">
-            <AiOutlineMail className="icon" />
-            <p className="contact"> palatare@palatare.com</p>
-          </li>
-          <li className="footerItem">
-            <BsTelephone className="icon" />
-            <p className="contact"> (47) 9 9877-9877</p>
-          </li>
-        </ul>
-      </footer>
-    );
-  }
-}
+const Footer = () => {
+  return (
+    <footer>
+      <img className="logo2" src={logo2} />
+      <ul className="footerList">
+        <li className="footerItem">
+          <MdLocationOn className="icon" />
+          <p className="contact">
+            Av. Brasil, 789 <br />
+            Baln. Camboriú | SC
+          </p>
+        </li>
+
+        <li className="footerItem">
+          <MdMail className="icon" />
+          <p className="contact"> palatare@palatare.com</p>
+        </li>
+
+        <li className="footerItem">
+          <BsFillTelephoneFill className="icon" />
+          <p className="contact"> (47) 9 9877-9877</p>
+        </li>
+      </ul>
+    </footer>
+  );
+};
 
 export default Footer;
