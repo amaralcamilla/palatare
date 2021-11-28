561fcfe4794d17f8a22be940b2925c6e272ae0de
Camilla <camillaamaral7@gmail.com>
2021-11-28 11:27:58 -0300

    Refatora Header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,16 @@
-import React from "react";
-import "./style.css";
-import logo1 from "../../img/logo1.png";
-import restaurante from "../../img/restaurante.png";
+import logo1 from "../../assets/logo1.png";
+import restaurante from "../../assets/restaurante.png";
 
-// class Header extends React.Component {
-//   render() {
-//     return (
-//       <header>
-//         <img className="logo" src={logo1} />
-//         <img className="restaurant" src={restaurante} />
-//         <h3>Economize e ganhe em saúde!</h3>
-
-//         <nav>
-//           {this.props.children}
-//         </nav>
-
-//       </header>
-//     );
-//   }
-// }
-
-const Header = ({children}) => {
+const Header = ({ children }) => {
   return (
     <header>
+      <img className="logo" src={logo1} />
+      <img className="restaurant" src={restaurante} />
+      <h3>Economize e ganhe em saúde!</h3>
 
-    <img className="logo" src={logo1} />
-    <img className="restaurant" src={restaurante} />
-    <h3>Economize e ganhe em saúde!</h3>
-
-    <nav>
-      {children}
-    </nav>
-
-  </header> 
-  )
-}
+      <nav>{children}</nav>
+    </header>
+  );
+};
 
 export default Header;
